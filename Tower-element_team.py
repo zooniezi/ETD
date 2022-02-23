4e78dd6c3875ad9a4aa20f1164ff32ca0a33558f
WooJ9 <dnwjdtn21@naver.com>
2022-02-23 22:07:14 +0900

    Add some Detail
    앞으로 진행할 일들을 주석으로 달아놨습니다

--- Tower-element_team.py
+++ Tower-element_team.py
@@ -29,8 +29,10 @@ class Tower:
 
             self.rangein = True
         
-
-
+    # def set_tower(self):
+        #랜덤으로 좌표를 받아서 
+        # pass
+       
 
     def reset(self):
         pass
@@ -53,21 +55,45 @@ class element:
         self.stun = st
         self.enchant = ec
 
-
+    # ----------원소 행동---------------
     def sell_element(self):
+        # gain_gold = self.price
+        # total_gold += self.price
+        # 원소가 없어지는 메소드
         pass
-    
+    def buy_element(self):
+        # total_gold -= self.price
+        # 상점에서 원소를 없애고 인벤토리로 추가해라
+        pass
+
+    def generate_element(self):
+        #inventory.append(dictionry.[원소번호-1])
+        #백과사전은 만들예정
+        pass
+
+
     def compose_element(self):
         pass
+        #인벤토리에서 합칠수도 있고 타워에서도 합칠 수 있다
+        #[tower]리스트 안에 원소가 있으면 그 원소는 없애고 새로운 원소를 만들어라. 
+        #인벤토리로 추가하는 함수
+        
+
+    def delete_element(self):
+        pass
+        #del(inventory)로 리스트를 받아서 없애기 아래에 예시?
+        #인벤토리 = [원소] 안에 원소를 없앤다.
+        #타워 = [원소]
 
 
+    # ---------원소 성질------------
     def with_slow(self):
         pass
 
     def with_dote(self):
-        if self.dote == True:
-            mob_hp -= 1
-
+        # if self.dote == True: 몇초동안 몇 대미지 넣을지 디테일 필요
+        # mop_hp -= 1
+        pass
 
     def with_Knock_back(self):
         pass
