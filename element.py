5617b61b6c8c24b21fabccea7171fa586ab4df37
zooniezi <75923015+zooniezi@users.noreply.github.com>
2022-03-19 13:55:00 +0900

    feat: elements
    save issue

--- element.py
+++ element.py
@@ -1,19 +1,28 @@
 class Element:
     def __init__(self):
         pass
+    def damageDealt(self, mob):
+        finalDamage = self.basicDamage - mob.armor
+        mob.hp -= finalDamage
 
 class Fire(Element):
-    def __init__(self, damage, price, attackSpeed, attackRange, splashRange, splashDamage):
+    def __init__(self, basicDamage, price, attackSpeed, attackRange, splashRange, splashDamage):
         super.__init__()
-        self.basicDamage = 1
+        self.basicDamage = 10
         self.price = 1
         self.attackSpeed = 1
         self.attackRange = 100
         self.splashRange = 50
         self.splashDamage = 10
-    
-    def finalDamage():
-        
 
-    def damageDealt(mob):
-        
+class Water(Element):
+    def __init__(self, basicDamage, price, attackSpeed, attackRange, slowPercentage):
+        super.__init__()
+        self.basicDamage = 5
+        self.price = 1
+        self.attackSpeed = 1
+        self.attackRange = 100
+        self.slowPercentage = 0.4
+
+    def slow(self, mob):
+        mob.speed = mob.speed*(1-self.slowPercentage)
