78eaacd44615de1db8e34e3c186d4487a12f9a70
zooniezi <75923015+zooniezi@users.noreply.github.com>
2022-03-19 13:49:07 +0900

    feat: elements
    add code of basic elements(Fire, Water)

---
+++ element.py
@@ -0,0 +1,19 @@
+class Element:
+    def __init__(self):
+        pass
+
+class Fire(Element):
+    def __init__(self, damage, price, attackSpeed, attackRange, splashRange, splashDamage):
+        super.__init__()
+        self.basicDamage = 1
+        self.price = 1
+        self.attackSpeed = 1
+        self.attackRange = 100
+        self.splashRange = 50
+        self.splashDamage = 10
+    
+    def finalDamage():
+        
+
+    def damageDealt(mob):
+        
